cc814c7fd1a4cfb407a3673d67007951269bf845
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-02-11 10:50:12 -0500

    Added sql file

---
+++ datafiles/schema-drop.sql
@@ -0,0 +1,11 @@
+-- OpenNSA SQL Schema (PostgreSQL) DROPs
+-- This is mainly for development
+
+DROP TABLE generic_backend_connections;
+DROP TABLE sub_connections;
+DROP TABLE service_connections;
+DROP TYPE directionality;
+DROP TYPE security_attribute;
+DROP TYPE parameter;
+DROP TYPE label;
+
