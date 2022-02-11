dd06d7dc0b788fd9fd95cd152c465926a309e828
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-02-11 10:47:57 -0500

    Moved SQL file

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ POSTGRES_DB=opennsa
 POSTGRES_USER=opennsa
 POSTGRES_PASSWORD=$(openssl rand -base64 18)
 
-SCHEMA_FILE=$PWD/../datafiles/schema.sql
+SCHEMA_FILE=$PWD/schema.sql
 
 OPENNSA_CONF_LOC=$PWD/config
 OPENNSA_TAC_LOC=$PWD/opennsa-docker.tac
